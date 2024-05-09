578cb1aeb747f83577e57d2fcc9f28857aa1eca4
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-05-09 23:11:04 +0200

    Create GroceryStore.cs

---
+++ InventoryManagement/GroceryStore.cs
@@ -0,0 +1,13 @@
+﻿namespace InventoryManagement
+{
+    // GroceryStore class
+    // This class represents the grocery store as a whole.
+    public class GroceryStore
+    {
+        // It has a property called Inventory that is an instance of the Inventory class.
+        public Inventory Inventory;
+
+        // Constructor
+        public GroceryStore() => Inventory = new Inventory();
+    }
+}
