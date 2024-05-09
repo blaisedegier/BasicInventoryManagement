094804b4a07d638f3932489d0ea2fc2d0c5a612b
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-05-09 23:10:11 +0200

    Create InputValidator.cs

---
+++ InventoryManagement/InputValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace InventoryManagement
+{
+    // InputValidator class
+    // This class is responsible for validating user input. It implements methods to validate user input for adding and removing items from the inventory.
+    class InputValidator
+    {
+        // ValidateItemAdd method
+        // Validate the name of the item
+        public static bool isValidName(string name) => name != null && name != "";
+        // Validate the price of the item
+        public static bool isValidPrice(double price) => price >= 0;
+        // Validate the quantity of the item
+        public static bool isValidQuantity(int quantity) => quantity >= 0;
+    }
+}
